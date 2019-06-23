285861676239ac2eef52678355856787b52efb90
Christopher Wong <christopherpk1996@gmail.com>
2019-06-24 00:52:51 +0800

    unset CI flag

--- client/src/pages/index-page/index-page.jsx
+++ client/src/pages/index-page/index-page.jsx
@@ -4,10 +4,6 @@ import { LocaleAction } from "../../redux/actions/locale-action";
 
 class Component extends React.Component {
 
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 			<React.Fragment>
